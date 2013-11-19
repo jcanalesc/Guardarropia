99b668e7f69d3755eb237ffafdae79c56634ff3f
Juan Canales <juancanalescpp@gmail.com>
2013-11-19 16:51:46 -0300

    version beta funcional

--- bdd.py
+++ bdd.py
@@ -4,6 +4,13 @@ import sqlite3
 
 app = Flask(__name__)
 
+def historial(cur,accion, **kwargs):
+	cur.execute("INSERT INTO historial (accion, codigo, idi, descr, lugar) VALUES (?,?,?,?,?)", (accion, 
+			kwargs.get("codigo", 0),
+			kwargs.get("idi", 0),
+			kwargs.get("descr", ""),
+			kwargs.get("lugar", 0)))
+
 @app.route("/")
 def index():
 	return render_template("index.html")
@@ -12,37 +19,61 @@ def index():
 def operacion(action):
 	with sqlite3.connect("guardarropia.db") as db:
 		db.row_factory = sqlite3.Row
-
 		cur = db.cursor()
 		codigo = request.args.get('codigo', None)
 		res = {
 			"items": [],
 			"available": -1,
-			"lastinsertion": 0,
-			"success": False
+			"lastinsertion": {},
+			"success": False,
+			"total" : 0
 		}
+		actionargs = {}
 		try:
 			if action == "get":
-				cur.execute("select * from guardarropia where codigo = ?", (codigo,))
+				cur.execute("select * from guardarropia where vigente = 1 and codigo = ?", (codigo,))
 				for row in cur:
 					res["items"].append(dict([(x, row[x]) for x in row.keys()]))
 			elif action == "add":
 				descr = request.args.get("descr", "")
 				lugar = request.args.get("lugar", "")
-				cur.execute("insert into guardarropia (codigo, descr, lugar) values (?,?,?)", (codigo, descr, lugar))
-				res["lastinsertion"] = cur.lastrowid
+				cur.execute("insert into guardarropia (codigo, descr, lugar, vigente) values (?,?,?, 1)", (codigo, descr, lugar))
+				res["lastinsertion"] = {
+					"id": cur.lastrowid,
+					"codigo": codigo,
+					"descr": descr,
+					"lugar": lugar
+				}
+				actionargs = {
+					"codigo": codigo,
+					"descr": descr,
+					"idi": cur.lastrowid,
+					"lugar": lugar
+				}
 			elif action == "mod":
 				idi = request.args.get("idi", "")
 				descr = request.args.get("descr", "")
-				cur.execute("update guardarropia set descr = ? where idi = ?", (descr, idi))
-			else: # rem
+				cur.execute("update guardarropia set descr = ? where id = ?", (descr, idi))
+				actionargs = {
+					"descr": descr,
+					"idi": idi
+				}
+			elif action == "rem": # rem
 				idi = request.args.get("idi", "")
-				cur.execute("delete from guardarropia where id = ?", (idi,))
-			cur.execute("select lugar from guardarropia group by lugar")
+				cur.execute("update guardarropia set vigente = 0 where id = ?", (idi,))
+				actionargs = {
+					"idi": idi
+				}
+			elif action == "remall": # remall
+				cur.execute("update guardarropia set vigente = 0")
+			cur.execute("select count(*) from guardarropia where vigente = 1")
+			res["total"] = cur.fetchone()[0]
+			cur.execute("select lugar from guardarropia where vigente = 1 group by lugar")
 			used = set([x[0] for x in cur])
 			res["available"] = min(set(range(1,1001)) - used)
+			historial(cur, action,**actionargs)
 			res["success"] = True
-		except sqlite3.IntegrityError:
+		except Exception:
 			res["success"] = False
 		return jsonify(res)
 
--- static/guardarropia.js
+++ static/guardarropia.js
@@ -1,19 +1,20 @@
 var DB = {
 	getPrendas: function(cod, callback)
 	{
-		if (cod.length > 0)
+		if (cod.length > 0 && /[0-9]+/.test(cod))
 		{
 			$.getJSON("/get/", {
 				codigo: cod
 			}, function(obj)
 			{
+				$("#sTotal").html(obj.total);
 				callback(obj);
 			});
 		}
 	},
 	addPrenda: function(cod, descr, lugar, callback)
 	{
-		if (descr.length > 0 && lugar.length > 0)
+		if (descr.length > 0 && /[0-9]+/.test(cod))
 		{
 			$.getJSON("/add/", {
 				codigo: cod,
@@ -21,6 +22,7 @@ var DB = {
 				lugar: lugar
 			}, function(obj)
 			{
+				$("#sTotal").html(obj.total);
 				callback(obj);
 			});
 		}
@@ -32,7 +34,8 @@ var DB = {
 			idi: idi,
 			descr: descr
 		}, function(res)
-		{	
+		{
+			$("#sTotal").html(res.total);
 			callback(res);
 		});
 	},
@@ -43,7 +46,16 @@ var DB = {
 			idi: idi
 		}, function(res)
 		{
+			$("#sTotal").html(res.total);
 			callback(res);
+		});
+	},
+	vaciar: function(callback)
+	{
+		$.getJSON("/remall/", function(res)
+		{
+			if (res.success)
+				callback(res);
 		});
 	}
 };
@@ -52,19 +64,55 @@ var $rowtemplate = null;
 var $emptytemplate = null;
 var $itemtemplate = null;
 var activepages = [];
+var buffer = "";
+function inputhandler2(ev)
+{
+	var selfref = $(this);
+	var rowref = $(this).parent().parent();
+	var tableref = rowref.parent();
+	var id = rowref.data("idi");
+	var codigo = tableref.data("codigo");
+	var buttonref = $(this).parent().next().next().find("button");
+	var itemref = tableref.parent().parent();
+	var descripcion = $(this).val();
+	DB.modPrenda(codigo, id, descripcion, function(res)
+	{
+		if (res.success)
+		{
+			//alert("modificada");
+			selfref.parent().find(".toggleable").toggle();
+			selfref.parent().find("span.toggleable").html(descripcion);
+		}
+	});
+}
 
+function inputhandler(ev)
+{
+	if (ev.which != 13) return;
+	$(this).blur();
+	ev.stopPropagation();
+}
 
 
 function addNewItemRow(item)
 {
 	DB.getPrendas("0", function(dbres)
 	{
-		$("tbody.itemlist", item.get()).append($itemtemplate
-				.replace("%descr%", "Agregue descripción")
-				.replace("%lugar%", dbres.available)
-				.replace("%idi%", "new")
-				.replace("%buttonlabel%", "Asignar"));
-		$("tbody.itemlist button").click(buttonhandler);
+		DB.addPrenda(item.data("codigo"), "Una prenda", dbres.available, function(res)
+		{
+			$("tbody.itemlist", item.get()).append($itemtemplate
+				.replace(/%descr%/g, res.lastinsertion.descr)
+				.replace("%lugar%", res.lastinsertion.lugar)
+				.replace("%idi%", res.lastinsertion.id)
+				.replace("%buttonlabel%", "Liberar"));
+			$("tbody.itemlist button", item.get()).click(buttonhandler);
+			$("tbody.itemlist input", item.get()).keydown(inputhandler).blur(inputhandler2);
+			$("tbody.itemlist span.toggleable", item.get()).click(function()
+			{
+				$(this).parent().find("span.toggleable").hide().parent().find("input.toggleable").show().select().focus();
+			}).show();
+			$("tbody.itemlist input.toggleable", item.get()).hide();
+		});
 	});
 }
 
@@ -76,42 +124,24 @@ function buttonhandler()
 	var codigo = tableref.data("codigo");
 	var buttonref = $(this);
 	var itemref = tableref.parent().parent();
-	if (id == "new")
+	DB.remPrenda(codigo, id, function(res)
 	{
-		var descripcion = $(this).parent().parent().find("input[type='text']").val();
-		var percha = $(this).parent().prev().text();
-		DB.addPrenda(codigo, descripcion, percha, function(res)
+		if (res.success)
 		{
-			if (res.success)
-			{
-				alert("agregada");
-				rowref.data("idi", res.lastinsertion);
-				buttonref.html("Liberar");
-			}
-		});
-	}
-	else
-	{
-		DB.remPrenda(codigo, id, function(res)
-		{
-			if (res.success)
-			{
-				alert("eliminada");
-				rowref.hide().remove();
-				console.log(tableref);
-				if (tableref.find("tr").length == 0)
-				{
-					console.log(itemref);
-					addNewItemRow(itemref);
-				}
+			//alert("eliminada");
+			rowref.hide().remove();
+			//console.log(tableref);
 
-			}
-		});
-	}
+		}
+	});
 }
 function procesaCodigo(cod)
 {
-	if (activepages.indexOf(cod) != -1) return;
+	if (activepages.indexOf(cod) != -1)
+	{
+		$("div[data-codigo='"+cod+"']").hide().remove();
+		activepages.splice(activepages.indexOf(cod));
+	};
 	DB.getPrendas(cod, function(res)
 	{
 		activepages.push(cod);
@@ -119,32 +149,51 @@ function procesaCodigo(cod)
 		for (var i = 0; i < res.items.length; i++)
 		{
 			$("tbody.itemlist", item.get()).append($itemtemplate
-				.replace("%descr%", res.items[i].descr)
+				.replace(/%descr%/g, res.items[i].descr)
 				.replace("%lugar%", res.items[i].lugar)
 				.replace("%idi%", res.items[i].id)
 				.replace("%buttonlabel%", "Liberar"));
 		}
-		if (res.items.length == 0)
-		{
-			addNewItemRow(item, res);
-		}
+		
 		$("button.close", item.get()).click(function()
 		{
-			$(this).parent().hide().remove();
+			$(this).parent().slideUp("slow", function()
+				{
+					$(this).remove();
+				});
 
 			activepages.splice(activepages.indexOf($(this).parent().data("codigo")));
 		});
-		$("button.actionbutton", item.get()).click(buttonhandler);
 		$(".bAgregar", item.get()).click(function()
 		{
-			console.log(this);
 			var itemr = $(this).parent();
 			addNewItemRow(itemr);
 		});
-
-		$("#iTabla").append(item);
-		item.slideDown("slow");
-
+		$("#iTabla").prepend(item);
+		$("tbody.itemlist button", item.get()).click(buttonhandler);
+		$("tbody.itemlist input", item.get()).keydown(inputhandler).blur(inputhandler2).select().focus();
+		$("tbody.itemlist span.toggleable").show().click(function()
+		{
+			$(this).parent().find("span.toggleable").hide().parent().find("input.toggleable").show().select().focus();
+		});
+		$("tbody.itemlist input.toggleable").hide();
+		if (res.items.length == 0)
+		{
+			item.slideDown("slow", function()
+			{
+				addNewItemRow(item);
+				$("input", item.get()).last().select().focus();
+			});
+		}
+		else
+		{
+			item.slideDown("slow");
+		}
+		$("input", item.get()).on("keydown keypress", function(ev)
+		{	
+			ev.stopPropagation();
+		});
+		
 	});
 }
 
@@ -152,19 +201,35 @@ $(function()
 {
 	$rowtemplate = $(".templateitem");
 	$emptytemplate = $(".templateempty");
-	$itemtemplate = "<tr data-idi='%idi%'><td><input type='text' class='form-control' value='%descr%' /></td><td>%lugar%</td><td><button class='btn actionbutton'>%buttonlabel%</button></td></tr>";
-	$("#iCodigo").focus();
-	$("#iCodigo").on("keyup", function(ev)
+	$itemtemplate = "<tr data-idi='%idi%'><td><span class='btn btn-block toggleable'>%descr%</span><input type='text' class='form-control toggleable' value='%descr%' /></td><td class='percha'>%lugar%</td><td><button class='btn btn-lg btn-info btn-block actionbutton'>%buttonlabel%</button></td></tr>";
+	$("#bVaciar").click(function()
 	{
-		if (ev.which == 13)
+		if (confirm("Realmente desea vaciar la guardarropía?"))
 		{
-			procesaCodigo($(this).val());
-			$(this).val("");
+			DB.vaciar(function()
+			{
+				alert("Guardarropía vaciada.");
+			});
 		}
 	});
-	$("#iCodigoBtn").click(function()
+	$(document).keypress(function(ev)
 	{
-		procesaCodigo($("#iCodigo").val());
-		$("#iCodigo").val("");
+		if (ev.which == 35)
+		{
+			buffer = "";
+		}
+		else if (ev.which == 13 && buffer != "")
+		{
+			procesaCodigo(buffer);
+			buffer = "";
+		}
+		else if (48 <= ev.which && ev.which <= 57) // numeros
+		{
+			buffer += String.fromCharCode(ev.which);
+		}
 	});
-});
+	$("input").on("keydown keypress", function(ev)
+	{	
+		ev.stopPropagation();
+	});
+});	
